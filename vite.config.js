c754eb095d6fc24f212305bfe2cb95df12d00e09
Vinay Gajula <glucose048@gmail.com>
2024-06-08 18:34:39 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['@emailjs/browser']
+      external: ['react-router-dom', '@emailjs/browser']
     }
   }
 })
